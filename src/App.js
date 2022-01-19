32a8f991a041f221e51a67f3447ad5fd6c7d005a
maiyzaza <maiy27982@gmail.com>
2022-01-20 02:56:33 +0700

    abc1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import React, { useRef } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import QuotationTable from "./QuotationTable";
 
--- src/QuotationTable.js
+++ src/QuotationTable.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Container, Row, Col, Table, Button } from "react-bootstrap";
 
 import { FaTrash } from 'react-icons/fa';
