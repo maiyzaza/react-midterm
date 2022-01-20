274aa232aee3efcfecfb7d206a62cd8fa22086d0
maiyzaza <maiy27982@gmail.com>
2022-01-20 10:51:24 +0700

    m

---
+++ build/precache-manifest.0ae06462baee4d8ff93e3675995a7de1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f57a864db20d7177bbdded20a40ea8f6",
+    "url": "/react-midterm/react-midterm-quotation/index.html"
+  },
+  {
+    "revision": "a825fd884d56fb1e1082",
+    "url": "/react-midterm/react-midterm-quotation/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "1721ca946e72299a74c8",
+    "url": "/react-midterm/react-midterm-quotation/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "a825fd884d56fb1e1082",
+    "url": "/react-midterm/react-midterm-quotation/static/js/2.c567c902.chunk.js"
+  },
+  {
+    "revision": "435881bb28fec4d4e84baf9dfd194218",
+    "url": "/react-midterm/react-midterm-quotation/static/js/2.c567c902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7c18f89fcd71cdb2d3d",
+    "url": "/react-midterm/react-midterm-quotation/static/js/3.9c9a075d.chunk.js"
+  },
+  {
+    "revision": "1721ca946e72299a74c8",
+    "url": "/react-midterm/react-midterm-quotation/static/js/main.aabe393e.chunk.js"
+  },
+  {
+    "revision": "f615ec8b88c6491fe057",
+    "url": "/react-midterm/react-midterm-quotation/static/js/runtime-main.15095608.js"
+  }
+]);
